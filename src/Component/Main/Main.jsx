41e0d86434fe71c748e41ed52d3d0992605ccc1d
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-20 03:36:04 +0600

    banner

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Section1 from '../Section1/Section1';
+// import Section2 from '../Section2/Section2';
 
 const Main = () => {
     return (
         <div>
             <Section1></Section1>
-            
+           
+
         </div>
     );
 };
