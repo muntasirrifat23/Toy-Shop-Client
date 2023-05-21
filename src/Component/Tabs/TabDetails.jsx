78c7ef049a31f28e09463952f891088b9c5335ef
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-21 10:15:59 +0600

    robot

--- src/Component/Tabs/TabDetails.jsx
+++ src/Component/Tabs/TabDetails.jsx
@@ -6,12 +6,21 @@ import 'react-tabs/style/react-tabs.css';
 const TabDetails = () => {
 
     const [allCars, setAllCars] = useState([]);
+    const [allRobot, setAllRobot] = useState([]);
 
-
+// 1
     useEffect(() => {
         fetch('https://server-11-lac.vercel.app/car')
             .then(res => res.json())
             .then(data => setAllCars(data))
+            .catch(err => console.log(err))
+    }, [])
+
+// 2
+    useEffect(() => {
+        fetch('https://server-11-lac.vercel.app/robot')
+            .then(res => res.json())
+            .then(data => setAllRobot(data))
             .catch(err => console.log(err))
     }, [])
 
@@ -20,10 +29,10 @@ const TabDetails = () => {
             <Tabs>
                 <TabList className=' bg-green-800 font-bold text-xl text-white rounded-xl'>
                     <Tab>Car Toy</Tab>
-                    <Tab>Sports Toy</Tab>
+                    <Tab>Robot Toy</Tab>
                     <Tab>Girls Toy</Tab>
                 </TabList>
-
+{/* 11111111111111111111111111111111111111111111111 */}
                 <TabPanel className='flex justify-center mt-5'>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 flex justify-center'>
                         {
@@ -51,17 +60,36 @@ const TabDetails = () => {
                             </p>)
                         }
                     </div>
-
-
-
                 </TabPanel>
 
-
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                    <h2>Any content 2</h2>
-                    <h2>Any content 2</h2>
+{/* 2222222222222222222222222222222222222222222222222 */}
+<TabPanel className='flex justify-center mt-5'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 flex justify-center'>
+                        {
+                            allRobot.map(robot => <p key={robot.id}>
+                                <div className="card w-96 bg-base-100 shadow-xl text-center">
+                                    <figure><img src={robot.image} alt="Shoes" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-2xl font-bold text-green-800">{robot.name}</h2>
+                                        <div className='flex'>
+                                            <p className='text-green-800 font-semibold text-lg card-title'>Price: ${robot.price}</p>
+                                            <h6 className='align-items-center text-red-900'>{robot.rating}
+                                                <Rating placeholderRating={robot.rating}
+                                                    readonly
+                                                    emptySymbol={<FaRegStar />}
+                                                    placeholderSymbol={<FaStar />}
+                                                    fullSymbol={<FaStar />}>
+                                                </Rating>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </p>)
+                        }
+                    </div>
                 </TabPanel>
+
+                {/* 3333333333333333333333333 */}
                 <TabPanel>
                     <h2>Any content 3</h2>
                     <h2>Any content 3</h2>
